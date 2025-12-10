2f9e576737cc02d136e3c0c76105489fce86108d
fansiuheiust <shfanab@connect.ust.hk>
2025-12-10 16:45:12 +0800

    actually deleted the mob validity check for mobs in mobroom.

--- Assets/Scripts/Dungeon/Room/MobRoom.cs
+++ Assets/Scripts/Dungeon/Room/MobRoom.cs
@@ -9,21 +9,13 @@ namespace Dungeon {
         void Awake() {
             _mobParent = transform.Find("Targets");
             for (int i = 0; i < _mobParent.childCount; i++) {
-                if (_mobParent.GetChild(i).TryGetComponent(out Mob m) && IsValidEnemy(m)) {
+                if (_mobParent.GetChild(i).TryGetComponent(out Mob m)) {
                     mobs.Add(m);
                     m.OnDeath.AddListener(OnMobDied);
                 }
             }
             if (mobs.Count == 0) throw new System.Exception("Mob room has no valid mobs");
         }
-
-        /// <summary>
-        /// See: returns. <br />
-        /// For now, this will return true for all cases. But in the future, some mobs should not be marked as to kill, e.g. allies.
-        /// </summary>
-        /// <param name="m">Self-documenting</param>
-        /// <returns>whether a mob should be in the to kill list</returns>
-        protected bool IsValidEnemy(Mob m) => true;
 
         /// <summary>
         /// Called once a mob dies, takes it out of the mob list, and mark the room as cleared if there are no mobs left
@@ -34,7 +26,6 @@ namespace Dungeon {
             mobs.Remove(m);
             if (mobs.Count == 0) {
                 Cleared = true;
-                Debug.Log("Cleared a room");
             }
         }
     }
