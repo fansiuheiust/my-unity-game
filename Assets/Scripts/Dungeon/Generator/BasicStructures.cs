a3a55e787c6a3d2fb287dd3cdaa1254dd656ccb1
fansiuheiust <shfanab@connect.ust.hk>
2025-12-14 22:29:39 +0800

    Added centering of rooms

--- Assets/Scripts/Dungeon/Generator/BasicStructures.cs
+++ Assets/Scripts/Dungeon/Generator/BasicStructures.cs
@@ -23,16 +23,26 @@ namespace Dungeon.Generator {
             new Vector2Int(coordinate.x, coordinate.y+1),
             new Vector2Int(coordinate.x, coordinate.y-1)
         };
+
+        public int HammingDistance(Block other) => System.Math.Abs(coordinate.x-other.coordinate.x) + System.Math.Abs(coordinate.y-other.coordinate.y);
     }
 
     class Room {
+        public Vector2Int Center { get; private set; }
         public List<Block> Blocks { get; private set; }
 
+        /// <summary>
+        /// Self-documenting
+        /// </summary>
+        /// <param name="center">Where (0,0) of <c>blocks</c> should be</param>
+        /// <param name="blocks">blocks with origin at (0,0), a version that is centered around <c>center</c> will be created for Room.Blocks</param>
+        /// <exception cref="System.Exception"></exception>
+        public Room(Vector2Int center, params Block[] blocks) {
+            Center = center;
+            Blocks = blocks.Select(b=>new Block(b.coordinate+center)).ToList();
 
-        public Room(Block[] blocks) {
-            Blocks = blocks.ToList();
-
-            // TODO: check if all blocks are vertically xor horizontally next to e.o.
+            if (Blocks.Count != 1 && Blocks.Any(x => !Blocks.Any(y => x.HammingDistance(y) == 1)))
+                throw new System.Exception("Blocks must be next to each other");
         }
         
         public List<Vector2Int> Edges {
@@ -50,7 +60,7 @@ namespace Dungeon.Generator {
 
     public static class Driver {
         public static void Main() {
-            new Room(new Block[] { new Block(new(0, 0)), new(new(0, 1)), new(new(1, 0)) }).Edges.ForEach(x=>Debug.Log(x));
+            new Room(new(5,4),new Block[] { new Block(new(0, 0)) }).Edges.ForEach(x=>Debug.Log(x));
         }
     }
 }
