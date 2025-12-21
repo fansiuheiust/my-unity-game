304e61b00731b162fd8bac92f34f4533bd99f112
fansiuheiust <shfanab@connect.ust.hk>
2025-12-21 18:52:18 +0800

    made the dungeon generator process data in 3D, while most components only span the x-z plane

--- Assets/Scripts/Dungeon/Generator/BasicStructures.cs
+++ Assets/Scripts/Dungeon/Generator/BasicStructures.cs
@@ -12,30 +12,30 @@ namespace Dungeon.Generator {
     /// <c>Final</c>: The room that leads you to the final boss<br />
     /// </summary>
     public enum RoomType {
-        Start, Mob, Final
+        Start, Mob, Final, Ladder
     }
 
     /// <summary>
     /// A square block
     /// </summary>
     struct Block {
-        public Vector2Int coordinate;
-        public Block(Vector2Int coordinate) {
+        public Vector3Int coordinate;
+        public Block(Vector3Int coordinate) {
             this.coordinate = coordinate;
         }
-        public Block(int x, int y) : this(new Vector2Int(x, y)) { }
+        public Block(int x, int z) : this(new Vector3Int(x, 0, z)) { }
 
         /// <summary>
         /// An array of the coordinates horizontally xor vertically next to the block
         /// </summary>
-        public Vector2Int[] Edges => new Vector2Int[] {
-            new Vector2Int(coordinate.x+1, coordinate.y),
-            new Vector2Int(coordinate.x-1, coordinate.y),
-            new Vector2Int(coordinate.x, coordinate.y+1),
-            new Vector2Int(coordinate.x, coordinate.y-1)
+        public Vector3Int[] Edges => new Vector3Int[] {
+            new Vector3Int(coordinate.x+1, coordinate.y, coordinate.z),
+            new Vector3Int(coordinate.x-1, coordinate.y, coordinate.z),
+            new Vector3Int(coordinate.x, coordinate.y, coordinate.z+1),
+            new Vector3Int(coordinate.x, coordinate.y, coordinate.z-1)
         };
 
-        public int HammingDistance(Block other) => System.Math.Abs(coordinate.x-other.coordinate.x) + System.Math.Abs(coordinate.y-other.coordinate.y);
+        public int HammingDistance(Block other) => System.Math.Abs(coordinate.x-other.coordinate.x) + System.Math.Abs(coordinate.z-other.coordinate.z);
     }
 
     /// <summary>
@@ -44,7 +44,7 @@ namespace Dungeon.Generator {
     class Room {
         public RoomType Type { get; private set; }
         public string Name { get; private set; }
-        Vector2Int _center;
+        Vector3Int _center;
 
         /// <summary>
         /// Self-documenting, you can assume that this must contain at least 1 element
@@ -64,16 +64,16 @@ namespace Dungeon.Generator {
         /// <param name="center">Where (0,0) of <c>blocks</c> should be</param>
         /// <param name="blocks">blocks with origin at (0,0), a version that is centered around <c>center</c> will be created for Room.Blocks.<br />An implicit (0,0) will be generated if not given</param>
         /// <exception cref="System.Exception">Thrown when blocks are invalid</exception>
-        public Room(Vector2Int center, string name, RoomType type, params Block[] blocks) {
+        public Room(Vector3Int center, string name, RoomType type, params Block[] blocks) {
 
             if (blocks is null || blocks.Length == 0)
                 blocks = new Block[1] { new Block(0,0) };
 
-            if (!blocks.Any(x=>x.coordinate==Vector2Int.zero))
+            if (!blocks.Any(x=>x.coordinate==Vector3Int.zero))
                 blocks = blocks.Append(new Block(0,0)).ToArray();
 
             if (blocks.Length != 1 && blocks.Any(x => !blocks.Any(y => x.HammingDistance(y) == 1)))
-                throw new System.Exception("Blocks must be next to each other horizontally XOR vertically");
+                throw new System.Exception("Blocks must be next to each other in x XOR z");
             Name = name;
             Blocks = blocks;
             Center = center;
@@ -86,31 +86,31 @@ namespace Dungeon.Generator {
         /// <param name="centerY">Where (0,0)'s y of <c>blocks</c> should be</param>
         /// <param name="blocks">blocks with origin at (0,0), a version that is centered around <c>center</c> will be created for Room.Blocks</param>
         /// <exception cref="System.Exception">Thrown when blocks are invalid</exception>
-        public Room(int centerX, int centerY, string name, RoomType type, params Block[] blocks) : this(new Vector2Int(centerX, centerY), name, type, blocks) { }
+        public Room(int centerX, int centerZ, string name, RoomType type, params Block[] blocks) : this(new Vector3Int(centerX, 0, centerZ), name, type, blocks) { }
 
         /// <summary>
         /// Checks whether the room occupies a coordinate
         /// </summary>
         /// <param name="coordinate">the coordinate to check</param>
         /// <returns>self-documenting</returns>
-        public bool Contains(Vector2Int coordinate) => Blocks.Any(b=>b.coordinate==coordinate);
+        public bool Contains(Vector3Int coordinate) => Blocks.Any(b=>b.coordinate==coordinate);
 
         public bool Collides(Room other) => Blocks.Any(b => other.Contains(b.coordinate));
 
         /// <summary>
         /// The list of edges 
         /// </summary>
-        public IEnumerable<Vector2Int> Edges => UnfilteredEdges
+        public IEnumerable<Vector3Int> Edges => UnfilteredEdges
                                                     .Distinct()
                                                     .Where(x => !Blocks.Any(b => b.coordinate == x));
         /// <summary>
         /// The list of edges in the room, WITHOUT filtering out overlaps and those inside of blocks, obviously faster (O(n))
         /// </summary>
-        public IEnumerable<Vector2Int> UnfilteredEdges => Blocks.Select(b => b.Edges)
+        public IEnumerable<Vector3Int> UnfilteredEdges => Blocks.Select(b => b.Edges)
                                                         .SelectMany(e=>e);
         
         
-        public Vector2Int Center {
+        public Vector3Int Center {
             get => _center;
             set {
                 for (int i = 0; i < Blocks.Length; i++)
@@ -128,17 +128,17 @@ namespace Dungeon.Generator {
                 value %= 4;
                 // rotate by 90*n degrees:
                 // [cosx, -sinx] [x]   [xcosx-ysinx]
-                // [sinx, cosx ] [y] = [xsinx+ycosx]
-                // 0: (+x, +y)
-                // 1: (-y, +x)
-                // 2: (-x, -y)
-                // 3: (+y, -x)
+                // [sinx, cosx ] [z] = [xsinx+ycosx]
+                // 0: (+x, +z)
+                // 1: (-z, +x)
+                // 2: (-x, -z)
+                // 3: (+z, -x)
                 uint toRotate = (4 + value - _normalizedRotation) % 4;
                 for (int i = 0; i < Blocks.Length; i++) {
                     ref Block b = ref Blocks[i];
                     b.coordinate -= Center;
-                    b.coordinate = new Vector2Int((toRotate % 3 == 0 ? 1 : -1) * (toRotate % 2 == 0 ? b.coordinate.x : b.coordinate.y),
-                                                  (toRotate < 2 ? 1 : -1) * (toRotate % 2 == 0 ? b.coordinate.y : b.coordinate.x))
+                    b.coordinate = new Vector3Int((toRotate % 3 == 0 ? 1 : -1) * (toRotate % 2 == 0 ? b.coordinate.x : b.coordinate.z), b.coordinate.y,
+                                                  (toRotate < 2 ? 1 : -1) * (toRotate % 2 == 0 ? b.coordinate.z : b.coordinate.x))
                                  + Center;
                 }
                 _normalizedRotation = value; 
@@ -153,12 +153,12 @@ namespace Dungeon.Generator {
         /// <summary>
         /// Vector that contains the smallest X and Y of the entire room
         /// </summary>
-        public Vector2Int MinCorner => new(Blocks.Min(b => b.coordinate.x), Blocks.Min(b => b.coordinate.y));
+        public Vector3Int MinCorner => new(Blocks.Min(b => b.coordinate.x), Blocks.Min(b => b.coordinate.y));
 
         /// <summary>
         /// Vector that contains the largest X and Y of the entire room
         /// </summary>
-        public Vector2Int MaxCorner => new(Blocks.Max(b => b.coordinate.x), Blocks.Max(b => b.coordinate.y));
+        public Vector3Int MaxCorner => new(Blocks.Max(b => b.coordinate.x), Blocks.Max(b => b.coordinate.y));
     }
 
     struct Connection {
@@ -169,9 +169,9 @@ namespace Dungeon.Generator {
         /// <summary>
         /// The coordinate of the connection point of the connectinf rooms
         /// </summary>
-        public Vector2 posA, posB;
+        public Vector3 posA, posB;
 
-        public Connection(Room a, Room b, Vector2 posA, Vector2 posB) {
+        public Connection(Room a, Room b, Vector3 posA, Vector3 posB) {
             this.a = a;
             this.b = b;
             this.posA = posA;
