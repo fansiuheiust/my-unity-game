6e19b3f0414f02bb3c35d4a487636f4b24efcd77
fansiuheiust <shfanab@connect.ust.hk>
2026-02-17 12:00:32 +0800

    Made a scriptable object for storing data related to player leveling

---
+++ Assets/Scripts/Progression/Balancing/Leveling.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using UnityEngine;
+namespace Progression.Balance {
+    [CreateAssetMenu(fileName = "Leveling", menuName = "Scriptable Objects/Leveling")]
+    public class Leveling : ScriptableObject {
+        [field: SerializeField]
+        public int MaxLevel { get; private set; } = 10;
+        [field: SerializeField, Tooltip("Determines how much progression points are needed to level up for each level.")]
+        public AnimationCurve LevelCurve { get; private set; }
+        [field: SerializeField, Tooltip("Multiplier of base stats of items for each level")]
+        public AnimationCurve BaseStatMultiplier { get; private set; }
+    }
+}
