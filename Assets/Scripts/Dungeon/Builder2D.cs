242d8db18d24abd9bccb1098a932dfd7964ccbd7
fansiuheiust <shfanab@connect.ust.hk>
2026-05-08 23:40:28 +0800

    Completed dungeon generator and wall generator, but clearing room does not open a gate

--- Assets/Scripts/Dungeon/Builder2D.cs
+++ Assets/Scripts/Dungeon/Builder2D.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using System.Linq;
 using Unity.VisualScripting;
+using Progression.Balance;
 
 namespace Dungeon {
     public class Builder2D : MonoBehaviour {
@@ -38,30 +39,80 @@ namespace Dungeon {
             var rooms = dungeon.GeneratedRooms;
             var connections = dungeon.GeneratedConnections;
             uint id = 0;
-            uint absLengthX = (uint)(dungeon.MaxCorner.x - dungeon.MinCorner.x+1), absLengthY = (uint)(dungeon.MaxCorner.y - dungeon.MinCorner.y+1);
-            uint[,] grid = new uint[absLengthX, absLengthY]; // logging ids for wall spawning
+            uint absLengthX = (uint)(dungeon.MaxCorner.x - dungeon.MinCorner.x+1), absLengthZ = (uint)(dungeon.MaxCorner.z - dungeon.MinCorner.z+1);
+            uint[,] grid = new uint[absLengthX, absLengthZ]; // logging ids for wall spawning
             for (uint i = 0; i < absLengthX; i++) {
-                for (uint j = 0; j < absLengthY; j++) {
+                for (uint j = 0; j < absLengthZ; j++) {
                     grid[i, j] = (uint)rooms.Count; // count signals empty room
                 }
             }
+            List<Room> generatedRooms = new();
             foreach (var (type, shape, blocks, center, rotation) in rooms) {
-                Vector3 spawnPos = (Vector3) center * (StageController.DungeonData.RoomLength + StageController.DungeonData.WallThickness);
                 
                 foreach (var block in blocks) {
-                    grid[block.x - dungeon.MinCorner.x, block.y - dungeon.MinCorner.y] = id; 
+                    grid[block.x - dungeon.MinCorner.x, block.z - dungeon.MinCorner.z] = id; 
                 }
+
+                Vector3 spawnPos = (Vector3)center * (StageController.DungeonData.RoomLength + StageController.DungeonData.WallThickness);
 
                 string path = $"Dungeon/Rooms/{type}/{shape}/";
                 var candidates = Resources.LoadAll(path);
                 GameObject spawnedRoom = (GameObject)Instantiate(candidates[Random.Range(0, candidates.Length-1)]);
-                spawnedRoom.transform.SetPositionAndRotation(spawnPos, Quaternion.Euler(0, rotation, 0));
+                spawnedRoom.transform.SetPositionAndRotation(spawnPos, Quaternion.Euler(0, -rotation, 0));
+                generatedRooms.Add(spawnedRoom.GetComponent<Room>());
 
                 id++;
 
             }
-        }
+            for (uint i = 0; i < absLengthX; i++) {
+                for (uint j = 0; j < absLengthZ; j++) {
+                    // vertical placement - edge of the dungeon
+                    if ((j == 0 || j == absLengthZ-1) && grid[i, j] != rooms.Count) {
+                        BuildWall((new Vector3(i, 0,j + (j==0 ? -0.5f: 0.5f))+dungeon.MinCorner)*(StageController.DungeonData.RoomLength+StageController.DungeonData.WallThickness), false, true);
+                    }
+                    // horizontal placement - edge of the dungeon
+                    if ((i == 0 || i == absLengthX-1) && grid[i, j] != rooms.Count) {
+                        BuildWall((new Vector3(i + (i==0 ? -0.5f: 0.5f), 0, j) + dungeon.MinCorner) * (StageController.DungeonData.RoomLength + StageController.DungeonData.WallThickness), false, false);
+                    }
 
+                    // vertical placement
+                    if (j < absLengthZ - 1 && grid[i,j] != grid[i,j+1]) {
+                        var c = connections.Where(x => x.posA == new Vector3Int((int)i, 0, (int)j) && x.posB == new Vector3Int((int)i, 0, (int)j + 1) || x.posB == new Vector3Int((int)i, 0, (int)j) && x.posA == new Vector3Int((int)i, 0, (int)j + 1));
+                        GameObject wall = BuildWall((new Vector3(i, 0, j + 0.5f) + dungeon.MinCorner) * (StageController.DungeonData.RoomLength + StageController.DungeonData.WallThickness),
+                            c.Count() > 0,
+                            true);
+                        if (c.Count() > 0) {
+                            var (a, b, posA, posB) = c.FirstOrDefault();
+                            wall.GetComponent<GatedWall>().AssignRooms(generatedRooms[a], generatedRooms[b]);
+                        }
+                    }
+
+                    // horizontal placement
+                    if (i < absLengthX - 1 && grid[i, j] != grid[i + 1, j]) {
+                        var c = connections.Where(x => x.posA == new Vector3Int((int)i, 0, (int)j) && x.posB == new Vector3Int((int)i+1, 0, (int)j) || x.posB == new Vector3Int((int)i, 0, (int)j) && x.posA == new Vector3Int((int)i+1, 0, (int)j));
+                        GameObject wall = BuildWall((new Vector3(i + 0.5f, 0, j) + dungeon.MinCorner) * (StageController.DungeonData.RoomLength + StageController.DungeonData.WallThickness),
+                            c.Count() > 0,
+                            false);
+                        if (c.Count() > 0) {
+                            var (a, b, posA, posB) = c.FirstOrDefault();
+                            wall.GetComponent<GatedWall>().AssignRooms(generatedRooms[a], generatedRooms[b]);
+                        }
+                    }
+
+                }
+            }
+        }
+        /// <summary>
+        /// Builds a wall at <c>position</c>
+        /// </summary>
+        /// <returns>The instantiated wall</returns>
+        /// <param name="isVertical">Vertical: |, Horizontal: -</param>
+        GameObject BuildWall(Vector3 position, bool isGated, bool isVertical) {
+            string path = $"Dungeon/Walls/{(isGated? "GatedWall": "UngatedWall")}";
+            GameObject wall = (GameObject)Instantiate(Resources.Load(path));
+            wall.transform.SetPositionAndRotation(position, Quaternion.Euler(0, isVertical? 90: 0, 0));
+            return wall;
+        }
 
     }
 }
